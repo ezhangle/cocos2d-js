7d30906610a107a51c6e24bb805a7254386c9c44
Alessar <alessarik@gmail.com>
2015-01-14 13:06:19 +0300

    Resolve issue with JS_GlobalObjectTraceHook

--- frameworks/js-bindings/bindings/manual/ScriptingCore.cpp
+++ frameworks/js-bindings/bindings/manual/ScriptingCore.cpp
@@ -429,7 +429,7 @@ static JSClass global_class = {
     "global", JSCLASS_GLOBAL_FLAGS,
     JS_PropertyStub, JS_DeletePropertyStub, JS_PropertyStub, JS_StrictPropertyStub,
     JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, sc_finalize,
-    0, 0, 0, 0, 0
+    0, 0, 0, JS_GlobalObjectTraceHook, JSCLASS_NO_INTERNAL_MEMBERS
 };
 
 ScriptingCore::ScriptingCore()
