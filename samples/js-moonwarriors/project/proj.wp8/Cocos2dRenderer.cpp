ea492d273921b472d75ea3bcd3beef8e494c802a
Alessar <alessarik@gmail.com>
2015-01-15 18:00:00 +0300

    Add code for WP8

---
+++ samples/js-moonwarriors/project/proj.wp8/Cocos2dRenderer.cpp
@@ -0,0 +1,181 @@
+﻿#include "pch.h"
+#include "Cocos2dRenderer.h"
+
+using namespace DirectX;
+using namespace Microsoft::WRL;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Core;
+
+USING_NS_CC;
+
+Cocos2dRenderer::Cocos2dRenderer() :
+	m_glView(nullptr),
+	m_AngleInitialized(false),
+	m_Initialized(false),
+	m_eglDisplay(EGL_NO_DISPLAY),
+	m_eglContext(EGL_NO_CONTEXT),
+	m_eglSurface(EGL_NO_SURFACE)
+{
+}
+
+void Cocos2dRenderer::Initialize(Windows::UI::Core::CoreWindow^ window)
+{
+	Direct3DBase::Initialize(window);
+
+	InitializeAngle();
+
+	CreateGLResources();
+
+	m_glView = GLViewImpl::sharedOpenGLView();
+}
+
+bool Cocos2dRenderer::InitializeAngle()
+{
+	// setup EGL
+	EGLint configAttribList[] = {
+		EGL_RED_SIZE,       8,
+		EGL_GREEN_SIZE,     8,
+		EGL_BLUE_SIZE,      8,
+		EGL_ALPHA_SIZE,     8,
+		EGL_DEPTH_SIZE,     8,
+		EGL_STENCIL_SIZE,   8,
+		EGL_SAMPLE_BUFFERS, 0,
+		EGL_NONE
+	};
+	EGLint surfaceAttribList[] = {
+		EGL_NONE, EGL_NONE
+	};
+
+	EGLint numConfigs;
+	EGLint majorVersion;
+	EGLint minorVersion;
+	EGLDisplay display;
+	EGLContext context;
+	EGLSurface surface;
+	EGLConfig config;
+	EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE, EGL_NONE };
+
+	// we need to select the correct DirectX feature level depending on the platform
+	// default is D3D_FEATURE_LEVEL_9_3 Windows Phone 8.0
+	ANGLE_D3D_FEATURE_LEVEL featureLevel = ANGLE_D3D_FEATURE_LEVEL::ANGLE_D3D_FEATURE_LEVEL_9_3;
+
+	switch(m_featureLevel)
+	{
+	case ANGLE_D3D_FEATURE_LEVEL_9_3:
+		featureLevel = ANGLE_D3D_FEATURE_LEVEL::ANGLE_D3D_FEATURE_LEVEL_9_3;
+		break;
+
+	case ANGLE_D3D_FEATURE_LEVEL_9_2:
+		featureLevel = ANGLE_D3D_FEATURE_LEVEL::ANGLE_D3D_FEATURE_LEVEL_9_2;
+		break;
+
+	case ANGLE_D3D_FEATURE_LEVEL_9_1:
+		featureLevel = ANGLE_D3D_FEATURE_LEVEL::ANGLE_D3D_FEATURE_LEVEL_9_1;
+		break;
+	}		
+
+	if(m_eglPhoneWindow == nullptr)
+	{
+		DX::ThrowIfFailed(
+			CreateWinPhone8XamlWindow(&m_eglPhoneWindow)
+			);
+	}
+
+	m_eglPhoneWindow->Update(m_d3dDevice.Get(), m_d3dContext.Get(), m_renderTargetView.Get());
+
+	ComPtr<IUnknown> u;
+	HRESULT r = m_eglPhoneWindow.As(&u);
+
+	if(m_eglWindow == nullptr)
+	{ 	
+		DX::ThrowIfFailed(
+			CreateWinrtEglWindow(u.Get(), featureLevel, m_eglWindow.GetAddressOf())
+			);
+	}
+
+	display = eglGetDisplay(m_eglWindow);
+	if(display == EGL_NO_DISPLAY){
+		//ofLogError("ofAppWinRTWindow") << "couldn't get EGL display";
+		return false;
+	}
+
+	if(!eglInitialize(display, &majorVersion, &minorVersion)){
+		//ofLogError("ofAppWinRTWindow") << "failed to initialize EGL";
+		return false;
+	}
+
+	// Get configs
+	if ( !eglGetConfigs(display, NULL, 0, &numConfigs) ){
+		//ofLogError("ofAppWinRTWindow") << "failed to get configurations";
+		return false;
+	}
+
+	// Choose config
+	if(!eglChooseConfig(display, configAttribList, &config, 1, &numConfigs)){
+		//ofLogError("ofAppWinRTWindow") << "failed to choose configuration";
+		return false;
+	}
+
+	// Create a surface
+	surface = eglCreateWindowSurface(display, config, m_eglWindow, surfaceAttribList);
+	if(surface == EGL_NO_SURFACE){
+		//ofLogError("ofAppWinRTWindow") << "failed to create EGL window surface";
+		return false;
+	}  
+
+	// Create a GL context
+	context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+	if(context == EGL_NO_CONTEXT){
+		//ofLogError("ofAppWinRTWindow") << "failed to create EGL context";
+		return false;
+	}   
+
+	// Make the context current
+	if (!eglMakeCurrent(display, surface, surface, context)){
+		//ofLogError("ofAppWinRTWindow") << "failed to make EGL context current";
+		return false;
+	}
+
+	m_eglDisplay = display;
+	m_eglSurface = surface;
+	m_eglContext = context;
+
+	m_AngleInitialized = true;
+	return true;
+}
+
+// Creates and restores Cocos2d-x after DirectX and Angle contexts are created or updated
+void Cocos2dRenderer::CreateGLResources()
+{
+	auto director = cocos2d::Director::getInstance();
+
+	if (!m_Initialized)
+	{
+		m_Initialized = true;
+		GLViewImpl* glview = GLViewImpl::create("js-moonwarriors");
+		director->setOpenGLView(glview);
+		glview->setDesignResolutionSize(m_renderTargetSize.Width, m_renderTargetSize.Height, ResolutionPolicy::SHOW_ALL);
+		glview->Create(m_eglDisplay, m_eglContext, m_eglSurface, m_renderTargetSize.Width, m_renderTargetSize.Height, m_orientation);
+		//glview->SetXamlEventDelegate(m_delegate);
+		//glview->SetXamlMessageBoxDelegate(m_messageBoxDelegate);
+		//glview->SetXamlEditBoxDelegate(m_editBoxDelegate);
+		CCApplication::getInstance()->run();
+	}
+	else
+	{
+		cocos2d::GL::invalidateStateCache();
+		cocos2d::ShaderCache::getInstance()->reloadDefaultGLPrograms();
+		cocos2d::DrawPrimitives::init();
+		cocos2d::VolatileTextureMgr::reloadAllTextures();
+		cocos2d::EventCustom recreatedEvent(EVENT_RENDERER_RECREATED);
+		director->getEventDispatcher()->dispatchEvent(&recreatedEvent);
+		cocos2d::Application::getInstance()->applicationWillEnterForeground();
+		director->setGLDefaultValues();
+	}
+}
+
+void Cocos2dRenderer::Render()
+{
+	if(m_glView)
+		m_glView->Render();
+}
---
+++ samples/js-moonwarriors/project/proj.wp8/PhoneApp.cpp
@@ -0,0 +1,140 @@
+﻿#include "PhoneApp.h"
+#include "ppltasks.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+
+PhoneApp::PhoneApp() :
+	m_windowClosed(false),
+	m_windowVisible(false)
+{
+}
+
+void PhoneApp::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &PhoneApp::OnActivated);
+	CoreApplication::Suspending +=
+		ref new EventHandler<SuspendingEventArgs^>(this, &PhoneApp::OnSuspending);
+	CoreApplication::Resuming +=
+		ref new EventHandler<Platform::Object^>(this, &PhoneApp::OnResuming);
+
+	m_renderer = ref new Cocos2dRenderer();
+}
+
+void PhoneApp::SetWindow(CoreWindow^ window)
+{
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &PhoneApp::OnVisibilityChanged);
+	window->Closed += 
+		ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &PhoneApp::OnWindowClosed);
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &PhoneApp::OnPointerPressed);
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &PhoneApp::OnPointerMoved);
+	window->PointerReleased +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &PhoneApp::OnPointerReleased);
+
+	m_renderer->Initialize(CoreWindow::GetForCurrentThread());
+}
+
+void PhoneApp::Load(Platform::String^ entryPoint)
+{
+}
+
+void PhoneApp::Run()
+{
+    while (!m_windowClosed)
+    {
+        if (m_windowVisible)
+        {
+            CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+            m_renderer->Render();  // This call is rendered picture into internal buffer.
+            m_renderer->Present(); // This call is synchronized internal buffer into display.
+        }
+        else
+        {
+            CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+        }
+    }
+}
+
+void PhoneApp::Uninitialize()
+{
+}
+
+void PhoneApp::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnVisibilityChanged(sender, args);
+}
+
+void PhoneApp::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	m_windowClosed = true;
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnWindowClosed(sender, args);
+}
+
+void PhoneApp::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnPointerPressed(sender, args);
+}
+
+void PhoneApp::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnPointerMoved(sender, args);
+}
+
+void PhoneApp::OnPointerReleased(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnPointerReleased(sender, args);
+}
+
+void PhoneApp::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void PhoneApp::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+	m_renderer->ReleaseResourcesForSuspending();
+	app.applicationDidEnterBackground();
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnSuspending(sender, args);
+	create_task([this, deferral]()
+	{
+		deferral->Complete();
+	});
+}
+ 
+void PhoneApp::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+	m_renderer->CreateWindowSizeDependentResources();
+	app.applicationWillEnterForeground();
+	if(m_renderer->m_glView)
+		m_renderer->m_glView->OnResuming(sender, args);
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+	return ref new PhoneApp();
+}
